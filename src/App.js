56e271fd2814282e72e478e8b0ea91a5c9e9b305
Shraddha <shraddha.belbase1@gmail.com>
2020-06-04 22:14:04 -0500

    layout

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ const dateBuilder = (d) => {
     </div>
     <div className= "weather-box">
     <div className= "temp">
-    {Math.round(weather.main.temp)}
+    {Math.round(weather.main.temp)}°C
     </div>
     <div className= "weather">{weather.weather[0].main}</div>
     </div>
